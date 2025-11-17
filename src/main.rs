cd2fac34423b14bbd5a0ec1d6300198533e52f38
Kurniawan Ade Putra <kurniawanadeputra17@gmail.com>
2025-11-17 12:47:27 +0700

    Belajar mutable

--- src/main.rs
+++ src/main.rs
@@ -8,3 +8,28 @@ fn main() {
 fn hello_test() {
     println!("Hello test");
 }
+
+#[test]
+fn test_variable(){
+    let name= "Kurniawan Ade Putra";
+    println!("Name: {}", name);
+}
+
+#[test]
+fn test_mutable_variable(){
+    let mut name =  "Kurniawan Ade Putra";
+    println!("Hello {}", name);
+
+    name = "Rahdatul Zaqia";
+    println!("Hello {}", name);
+}
+
+#[test]
+fn static_typing(){
+    let mut name =  "Kurniawan Ade Putra";
+    println!("Hello {}", name);
+
+    // name = 10;
+    
+    println!("Hello {}", name);
+}
