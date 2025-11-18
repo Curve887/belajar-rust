73b06371662a51ba602dbbeed738f63e8254f07e
Kurniawan Ade Putra <kurniawanadeputra17@gmail.com>
2025-11-18 10:39:38 +0700

    Belajar tuple dan tuple mutable

--- src/main.rs
+++ src/main.rs
@@ -145,3 +145,23 @@ fn char_type() {
 
     println!("{} {}", char1, char2); 
 }
+
+#[test]
+fn tuple() {
+    let mut data: (i32, f64, bool) = (10, 10.5, true);
+    println!("{:?}", data);
+
+    /* 
+    // let a = data.0;
+    // let b = data.1;      
+    // let c = data.2;
+    */
+
+    let  (a, b, c) = data; // destructuring
+    println!("{} {} {}", a, b, c);
+
+    data.0 = 20; // mutable
+    data.1 = 20.5; // mutable
+    data.2 = false; //mutable   
+    println!("{:?}", data);
+}
