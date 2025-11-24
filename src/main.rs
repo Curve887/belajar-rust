b54f4a5cbd99b028067044091a612fa71a022dab
Kurniawan Ade Putra <kurniawanadeputra17@gmail.com>
2025-11-24 13:59:16 +0700

    Belajar Return Value

--- src/main.rs
+++ src/main.rs
@@ -452,3 +452,23 @@ fn test_goodbye() {
     say_goodbye("Joko ", "Susilo");
 }
 
+fn factorial_loop(n: i32) -> i32 {
+    if n < 1 {
+        return 0;
+    }
+    
+    let mut result = 1;
+    for i in 1..=n {
+        result *= i;
+    }
+    result   
+}
+
+#[test]
+fn test_factorial_loop() {
+    let result: i32 = factorial_loop(5);
+    println!("{}", result);
+
+    let result: i32 = factorial_loop(-10);
+    println!("{}", result);
+}
